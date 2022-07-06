48019404f0cb780731fb8c049769e3d355507b8f
Perez <luis-alberto.perez@atos.net>
2022-07-06 11:23:53 -0500

    sorted table by country name

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ fetch(url).then((response)=>{
     return response.json();
 }).then(data=>{
 
-    console.log(data);
+    // console.log(data);
     data.forEach(function(element) {
         countryTable.insertAdjacentHTML('beforeend', 
-            "<tr><th>" + element.name.official + "</th>"
+            "<tr><th id='name'>" + element.name.official + "</th>"
             + "<td id='capitals'>" + getCapital(element.capital) + "</td>"
             + "<td id='region'>" + element.region + "</td>"
             + "<td id='languages'>" + getLanguages(element.languages) + "</td>"
@@ -17,6 +17,7 @@ fetch(url).then((response)=>{
             + "</tr>"
         );
     });
+    sortTable("ASC");
   
 }).catch(error=>{
     console.log(error);
@@ -37,3 +38,40 @@ function getCapital(capital){
         return "No capital";
     }   
 }
+
+function sortTable(){
+    var rows = countryTable.rows;
+    for (var i = 0; i < rows.length-1; i++){
+        var rowOne = rows[i];
+        var rowTwo = rows[i+1];
+        console.log(rowOne);
+    }
+}
+
+function sortTable(order) {
+    var switching, x, y, shouldSwitch;
+    
+    switching = true;
+
+    while (switching) {
+      switching = false;
+      var rows = countryTable.rows;
+ 
+      for (var i = 0; i < rows.length-1; i++) {
+        shouldSwitch = false;
+        x = rows[i].getElementsByTagName("th")[0];
+        y = rows[i+1].getElementsByTagName("th")[0];    
+
+        if(order == "ASC"){
+            if (x.innerHTML > y.innerHTML) {
+                shouldSwitch = true;
+                break;
+            }
+        }
+      }
+      if (shouldSwitch) {
+        rows[i].parentNode.insertBefore(rows[i+1], rows[i]);
+        switching = true;
+      }
+    }
+  }
