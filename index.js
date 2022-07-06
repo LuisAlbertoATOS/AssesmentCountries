2804ae09519cb0a04d1e2f853adc688fd88545df
Perez <luis-alberto.perez@atos.net>
2022-07-06 10:47:46 -0500

    created table to display all values

--- index.js
+++ index.js
@@ -1,16 +1,39 @@
 const url = "https://restcountries.com/v3.1/all"
-const divList = document.querySelector("#app");
+const countryTable = document.querySelector("#contentTable");
 
 fetch(url).then((response)=>{
     return response.json();
 }).then(data=>{
-  
-    // console.log(data);
+
+    console.log(data);
     data.forEach(function(element) {
-        // console.log(element.name.common);
-        divList.insertAdjacentHTML('beforeend', "<li>" + element.name.common + " </li>");
+        countryTable.insertAdjacentHTML('beforeend', 
+            "<tr><th>" + element.name.official + "</th>"
+            + "<td id='capitals'>" + getCapital(element.capital) + "</td>"
+            + "<td id='region'>" + element.region + "</td>"
+            + "<td id='languages'>" + getLanguages(element.languages) + "</td>"
+            + "<td id='population'>" + element.population + "</td>"
+            + "<td id='flag'>" + element.flag + "</td>"
+            + "</tr>"
+        );
     });
   
 }).catch(error=>{
     console.log(error);
 })
+
+function getLanguages(languages){
+    if (languages){
+        return Object.values(languages);
+    } else {
+        return "No language to display";
+    }
+}
+
+function getCapital(capital){
+    if (capital){
+        return capital;
+    } else {
+        return "No capital";
+    }   
+}
